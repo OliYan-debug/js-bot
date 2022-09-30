37f46c8d1a8624944340859a648a19902e23f8b1
oliyan-debug <77648737+OliYan-debug@users.noreply.github.com>
2022-09-30 13:38:32 -0300

    Fixing broken commands

--- src/modules/player.js
+++ src/modules/player.js
@@ -14,60 +14,67 @@ const player = createAudioPlayer({
 });
 
 function joinChannel(message) {
-    let channelId = message.member?.voice.channelId || "";
-    let guildId = message.member?.guild.id || "";
-    let adapterCreator = message.member?.guild.voiceAdapterCreator || "";
+  try {
+    let channelId = message.member.voice.channelId;
+    let guildId = message.member.guild.id;
+    let adapterCreator = message.member.guild.voiceAdapterCreator;
     const connection = joinVoiceChannel({
       channelId,
       guildId,
       adapterCreator: adapterCreator,
     });
     return connection;
+  } catch {
+    return false;
   }
-
+}
 
 const botPlayer = {
   playMusic: async (message, command, args) => {
-  
-    const commands = ['play', 'pause', 'unpause']
-    if (!commands.includes(command)){
-      return
+    const commands = ["play", "pause", "unpause"];
+    if (!commands.includes(command)) {
+      return;
     }
 
+    if (command === "play") {
+      const conn = joinChannel(message);
+      if (con) {
+        const vidFind = async (query) => {
+          const vidRes = await ytSearch(query);
+          return vidRes.videos.length > 1 ? vidRes.videos[0] : null;
+        };
+        const video = await vidFind(args.join(""));
+        if (video) {
+          const stream = ytdl(video.url, { filter: "audioonly" });
+          const resource = createAudioResource(stream, {
+            inputType: StreamType.Arbitrary,
+          });
 
-  if (command === "play") {
-    const conn = joinChannel(message);
-    const vidFind = async (query) =>{
-        const vidRes = await ytSearch(query);
-        return (vidRes.videos.length > 1) ? vidRes.videos[0] : null;
-    }
-    const video = await vidFind(args.join(""));
-    if(video){
-       const stream = ytdl(video.url, { filter: "audioonly" })
-       const resource = createAudioResource(stream, {
-        inputType: StreamType.Arbitrary,
-      });
-  
-      player.play(resource);
-      conn.subscribe(player);
-      
-      message.reply(`Tocando **${video.title}**` + " digite `!pause` ou `!stop` para aplicar comandos")
+          player.play(resource);
+          conn.subscribe(player);
 
+          message.reply(
+            `Tocando **${video.title}**` +
+              " digite `!pause` ou `!stop` para aplicar comandos"
+          );
+        }
+      }
+      if (command === "pause") {
+        player.pause();
+        message.reply("Música pausada digite `!unpause` para continuar");
+      }
+      if (command === "unpause") {
+        player.unpause();
+        message.reply("A festa continua");
+      }
+      if (command === "stop") {
+        player.stop();
+        conn.destroy();
+        message.reply("Fim da festa!");
+      }
+    } else {
+      message.reply("Você tem que entrar em um **canal de áudio** primeiro");
     }
-    
-  }
-  if (command === "pause") {
-    player.pause();
-    message.reply('Música pausada digite `!unpause` para continuar')
-  }
-  if (command === "unpause") {
-    player.unpause();
-    message.reply('A festa continua')
-  }
-  if (command === "stop") {
-    conn.destroy();
-    message.reply('Fim da festa!')
-  }
-}
-}
-module.exports = botPlayer
+  },
+};
+module.exports = botPlayer;
